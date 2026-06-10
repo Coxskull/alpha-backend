243a348329eb318a57668fb042996b669a5f4c04
Coxskull <119336582+Coxskull@users.noreply.github.com>
2026-06-10 21:11:52 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(
-        builder.Configuration.GetConnectionString("DefaultConnection")
+        builder.Configuration["DATABASE_URL"]
     ));
 
 builder.Services.AddCors(options =>
