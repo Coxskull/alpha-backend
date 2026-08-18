246be06d14a44ddb4366ea404c90e448fb1b4e2e
Erwin De Jesus <dejesuserwin1@gmail.com>
2026-08-18 21:52:46 +0800

    sdsa

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -336,10 +336,7 @@ public class AppDbContext : DbContext
         {
             entity.ToTable("entrepreneur_program_configurations");
 
-            entity.HasKey(x => x.Id);
-
-            entity.Property(x => x.Id)
-                .HasColumnName("id");
+            entity.HasKey(x => x.id);
 
             entity.Property(x => x.ProgramEnabled)
                 .HasColumnName("program_enabled");
